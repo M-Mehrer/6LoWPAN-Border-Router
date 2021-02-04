e8e3e23d82c69a6b384b5fd93b7d3b9c70a60a84
M-Mehrer <marcmehrer@t-online.de>
2021-02-04 19:14:44 +0100

    test other ip

--- server_zero_1.py
+++ server_zero_1.py
@@ -2,7 +2,7 @@ import socket
 
 SERVER_PORT = 9000
 #insert ipv6 address of your device
-SERVER_ADDR = 'fd76:afe2:b6b0:0:b827:ebff:fe5e:6de'
+SERVER_ADDR = 'fd76:afe2:b6b0:1:b827:ebff:fe5e:6de'
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr = (SERVER_ADDR, SERVER_PORT, 0, 0)
 sock.bind(server_addr)
