4360f9e594c92f49ff06eba94ed7fe6a83af0397
M-Mehrer <marcmehrer@t-online.de>
2021-02-04 18:57:28 +0100

    added startup message

--- server_zero_1.py
+++ server_zero_1.py
@@ -6,6 +6,7 @@ SERVER_ADDR = 'fd76:afe2:b6b0:0:b827:ebff:fe5e:6de'
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr = (SERVER_ADDR, SERVER_PORT, 0, 0)
 sock.bind(server_addr)
+print("Server started...")
 while 1 :
     recv_data = sock.recvfrom(8)
     print(recv_data)
