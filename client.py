cad930b6c6348b8ff6466e35fb84ff68455e1680
M-Mehrer <marcmehrer@t-online.de>
2021-03-06 00:46:02 +0100

    updated ipv6 addresses to use pi as client

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 
 #insert ipv6 address of your device
-SERVER_ADDR = 'fd76:afe2:b6b0:0:b827:ebff:fe5e:6de'
-SERVER_PORT = 9000
+SERVER_ADDR = 'fdee:f1e7:b9b5:0:8525:7c01:71c0:6180'
+SERVER_PORT = 9013
 sequence_number = 1
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 payload = "Ping"
@@ -10,7 +10,9 @@ while sequence_number < 101 :
     sequence_payload = payload + str(sequence_number)
     data = bytes(sequence_payload, "UTF-8")
     sock.sendto(data, (SERVER_ADDR, SERVER_PORT, 0, 0))
+    print("sent...")
     recv_data = sock.recvfrom(8)
+    print("received...")
     print(recv_data)
     rx_sequence_payload = recv_data[0]
 
--- server_zero_1.py
+++ server_zero_1.py
@@ -1,8 +1,8 @@
 import socket
 
-SERVER_PORT = 9000
+SERVER_PORT = 9013
 #insert ipv6 address of your device
-SERVER_ADDR = 'fd76:afe2:b6b0:0:b827:ebff:fe5e:6de'
+SERVER_ADDR = 'fdee:f1e7:b9b5:0:8525:7c01:71c0:6180'
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr = (SERVER_ADDR, SERVER_PORT, 0, 0)
 sock.bind(server_addr)
