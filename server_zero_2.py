820f652f508a6d859721bd76278d9acfb1fbf88a
M-Mehrer <marcmehrer@t-online.de>
2021-03-06 00:33:07 +0100

    Update IPv6 address

--- server_zero_2.py
+++ server_zero_2.py
@@ -1,8 +1,8 @@
 import socket
 
-SERVER_PORT = 9000
+SERVER_PORT = 9013
 #insert ipv6 address of your device
-SERVER_ADDR = 'fd76:afe2:b6b0:0:b827:ebff:fe5e:6de'
+SERVER_ADDR = 'fdee:f1e7:b9b5:10:b827:ebff:fe5e:6de'
 sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr = (SERVER_ADDR, SERVER_PORT, 0, 0)
 sock.bind(server_addr)
