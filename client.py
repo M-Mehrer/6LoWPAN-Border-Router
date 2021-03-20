f807a69d3f48f5575d954172bb0573c9c867754c
M-Mehrer <marcmehrer@t-online.de>
2021-03-20 17:17:23 +0100

    WLAN only v2

--- client.py
+++ client.py
@@ -1,21 +1,21 @@
 import socket
 import time
 
-SERVER_PORT_BLE = 9010
-SERVER_ADDR_BLE = 'fdee:f1e7:b9b5:10:b827:ebff:fe5e:6de'
-ble_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-server_addr_ble = (SERVER_ADDR_BLE, SERVER_PORT_BLE, 0, 0)
+# SERVER_PORT_BLE = 9010
+# SERVER_ADDR_BLE = 'fdee:f1e7:b9b5:10:b827:ebff:fe5e:6de'
+# ble_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+# server_addr_ble = (SERVER_ADDR_BLE, SERVER_PORT_BLE, 0, 0)
 
 SERVER_PORT_WLAN = 9020
 SERVER_ADDR_WLAN = '2003:e5:2722:bf00:2245:adce:3308:888c'
 wlan_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr_wlan = (SERVER_ADDR_WLAN, SERVER_PORT_WLAN, 0, 0)
 
-RESPONSE_PORT = 9030
-RESPONSE_ADDR = '2003:e5:2722:bf00:855b:ebe4:2980:1e7c'
-response_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-response_addr = (RESPONSE_ADDR, RESPONSE_PORT, 0, 0)
-response_socket.bind(response_addr)
+# RESPONSE_PORT = 9030
+# RESPONSE_ADDR = '2003:e5:2722:bf00:855b:ebe4:2980:1e7c'
+# response_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+# response_addr = (RESPONSE_ADDR, RESPONSE_PORT, 0, 0)
+# response_socket.bind(response_addr)
 
 FILENAME = "TestPayload"
 
--- server.py
+++ server.py
@@ -1,10 +1,10 @@
 import socket
 
-SERVER_PORT_BLE = 9010
-SERVER_ADDR_BLE = 'fdee:f1e7:b9b5:10:b827:ebff:fe5e:6de'
-ble_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-server_addr_ble = (SERVER_ADDR_BLE, SERVER_PORT_BLE, 0, 0)
-ble_socket.bind(server_addr_ble)
+# SERVER_PORT_BLE = 9010
+# SERVER_ADDR_BLE = 'fdee:f1e7:b9b5:10:b827:ebff:fe5e:6de'
+# ble_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+# server_addr_ble = (SERVER_ADDR_BLE, SERVER_PORT_BLE, 0, 0)
+# ble_socket.bind(server_addr_ble)
 
 SERVER_PORT_WLAN = 9020
 SERVER_ADDR_WLAN = '2003:e5:2722:bf00:2245:adce:3308:888c'
@@ -12,10 +12,10 @@ wlan_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
 server_addr_wlan = (SERVER_ADDR_WLAN, SERVER_PORT_WLAN, 0, 0)
 wlan_socket.bind(server_addr_wlan)
 
-RESPONSE_PORT = 9030
-RESPONSE_ADDR = '2003:e5:2722:bf00:855b:ebe4:2980:1e7c'
-response_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-response_addr = (RESPONSE_ADDR, RESPONSE_PORT, 0, 0)
+# RESPONSE_PORT = 9030
+# RESPONSE_ADDR = '2003:e5:2722:bf00:855b:ebe4:2980:1e7c'
+# response_socket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+# response_addr = (RESPONSE_ADDR, RESPONSE_PORT, 0, 0)
 
 print("Server started...")
 
